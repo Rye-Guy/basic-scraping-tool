6ad8fc0781babd0b622e2e086c512d49d41af45a
Rye-Guy <ryanracioppo95@gmail.com>
2019-03-12 12:10:57 -0400

    better structure for base scraping tool

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.quora.com/What-are-packages-in-Node-js-to-scrape-a-website-which-has-dynamically-added-content-by-Javascript');
-    await page.screenshot({path: 'example.png'});
-    await browser.close();
-})();
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage()
+    await page.goto('http://somewebsite.com/')
+    await page.waitFor(1000)
+    
+    
+    //Insert scaping code here
+    
+
+    browser.close()
+    return result; 
+};
+
+scrape().then((value)=>{
+    console.log(value)
+})
