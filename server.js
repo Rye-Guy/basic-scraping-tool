9a0f796c7251c2e5166082af2c19d1b4db76ea78
Rye-Guy <ryanracioppo95@gmail.com>
2019-03-12 16:15:03 -0400

    base code in place, export to scraped data into csv

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
 
 let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
+    let allLinks = []
+    const browser = await puppeteer.launch();
     const page = await browser.newPage()
-    await page.goto('http://somewebsite.com/')
-    await page.waitFor(1000)
-    
-    
-    //Insert scaping code here
-    
-
+    await page.goto(`https://www.thebestwebsitetoscrapeevar!!!.com/awesome`)     
+    let hrefs = await page.evaluate(() => {
+        const links = Array.from(document.querySelectorAll('someselectorforwhatyouwanttoscrape'))
+        return links.map(link => link.href);
+    });
+    allLinks += hrefs + ','
     browser.close()
-    return result; 
+    return allLinks; 
 };
 
 scrape().then((value)=>{
-    console.log(value)
+    myarr = value.split(',')
+    const filename = path.join(__dirname, 'output.csv');
+    const output = []
+    myarr.forEach((link) => {
+        const row = [];
+        row.push(link)
+        output.push(row.join());
+      });
+    fs.writeFileSync(filename, output.join(os.EOL));
 })
